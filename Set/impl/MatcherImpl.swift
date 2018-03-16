4ca76dd3f21636d1dd26e89ced80d8383c37c103
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-03-16 10:33:26 +0000

    Implement Matcher

--- Set/impl/MatcherImpl.swift
+++ Set/impl/MatcherImpl.swift
@@ -6,6 +6,50 @@ class MatcherImpl: Matcher {
         if (cards.count != 3) {
             return false
         }
+        if (!numbersMatch(cards)) {
+            return false
+        }
+        if (!shapesMatch(cards)) {
+            return false
+        }
+        if (!colorMatch(cards)) {
+            return false
+        }
+        if (!shadingMatch(cards)) {
+            return false
+        }
         return true
+    }
+
+    private func shadingMatch(_ cards: [Card]) -> Bool {
+        let numbers = cards.map { card in
+            card.shading
+        }
+        let set = Set<Shape>(numbers)
+        return (set.count == 3 || set.count == 1)
+    }
+
+    private func colorMatch(_ cards: [Card]) -> Bool {
+        let numbers = cards.map { card in
+            card.color
+        }
+        let set = Set<Color>(numbers)
+        return (set.count == 3 || set.count == 1)
+    }
+
+    private func shapesMatch(_ cards: [Card]) -> Bool {
+        let numbers = cards.map { card in
+            card.shape
+        }
+        let set = Set<Shape>(numbers)
+        return (set.count == 3 || set.count == 1)
+    }
+
+    private func numbersMatch(_ cards: [Card]) -> Bool {
+        let numbers = cards.map { card in
+            card.number
+        }
+        let set = Set<Int>(numbers)
+        return (set.count == 3 || set.count == 1)
     }
 }
--- SetTests/MatcherTest.swift
+++ SetTests/MatcherTest.swift
@@ -23,6 +23,76 @@ public class MatcherTest: XCTestCase {
         XCTAssert(!matcher!.isAMatch(cards: cards))
     }
 
+    public func test_allSame() {
+        putAllSame()
+        XCTAssert(matcher!.isAMatch(cards: cards))
+    }
+
+    public func test_numbersDoNotMatch() {
+        numbersDoNotMatch()
+        XCTAssert(!matcher!.isAMatch(cards: cards))
+    }
+
+    public func test_shapeDoNotMatch() {
+        shapeDoNotMatch()
+        XCTAssert(!matcher!.isAMatch(cards: cards))
+    }
+
+    public func test_colorDoNotMatch() {
+        colorDoNotMatch()
+        XCTAssert(!matcher!.isAMatch(cards: cards))
+    }
+
+    public func test_shadingDoNotMatch() {
+        shadingDoNotMatch()
+        XCTAssert(!matcher!.isAMatch(cards: cards))
+    }
+
+    private func shadingDoNotMatch() {
+        let card = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.FIRST)
+        let card1 = Card(number: 1, shape: Shape.FIRST, color: Color.SECOND, shading: Shading.SECOND)
+        let card2 = Card(number: 1, shape: Shape.FIRST, color: Color.THIRD, shading: Shading.SECOND)
+        cards.append(card)
+        cards.append(card1)
+        cards.append(card2)
+    }
+
+    private func colorDoNotMatch() {
+        let card = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.FIRST)
+        let card1 = Card(number: 1, shape: Shape.FIRST, color: Color.SECOND, shading: Shading.SECOND)
+        let card2 = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.THIRD)
+        cards.append(card)
+        cards.append(card1)
+        cards.append(card2)
+    }
+
+    private func shapeDoNotMatch() {
+        let card = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.FIRST)
+        let card1 = Card(number: 1, shape: Shape.SECOND, color: Color.FIRST, shading: Shading.SECOND)
+        let card2 = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.THIRD)
+        cards.append(card)
+        cards.append(card1)
+        cards.append(card2)
+    }
+
+    private func putAllSame() {
+        let card = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.FIRST)
+        let card1 = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.SECOND)
+        let card2 = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.THIRD)
+        cards.append(card)
+        cards.append(card1)
+        cards.append(card2)
+    }
+
+    private func numbersDoNotMatch() {
+        let card = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.FIRST)
+        let card1 = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.SECOND)
+        let card2 = Card(number: 2, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.THIRD)
+        cards.append(card)
+        cards.append(card1)
+        cards.append(card2)
+    }
+
     private func putTooMany() {
         let card = Card(number: 1, shape: Shape.FIRST, color: Color.FIRST, shading: Shading.FIRST)
         let card1 = Card(number: 2, shape: Shape.SECOND, color: Color.SECOND, shading: Shading.SECOND)
